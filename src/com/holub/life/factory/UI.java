56e8dceb450baec7edeef2d5f147ef81c64fb106
LSMG3 <81462890+LSMG3@users.noreply.github.com>
2022-12-01 00:32:49 +0900

    Reflect feedback

--- src/com/holub/life/factory/UI.java
+++ src/com/holub/life/factory/UI.java
@@ -1,29 +1,113 @@
 package com.holub.life.factory;
 
 import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
+import javax.swing.JPanel;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+
+import com.holub.io.Files;
 import com.holub.life.Cell;
+import com.holub.life.Clock;
+import com.holub.life.Storable;
 
-public class UI extends JFrame {
-	//JFrame¿∏∑Œ ∫Ø∞Ê
+public abstract class UI extends JPanel {
 	
-	private GameCell gamecell;
-	public Cell outermostCell = gamecell.makeCell();
+	public GameCell gamecell;
 	
+	public void UI(GameCell gc) {
+		this.gamecell = gc;
+	}
 	
-	public void makeUI() {};
+	public void makeUI() {}
 	
-	//Clockø° ¿÷¥¯∞≈
-	public void createMenus() {};
+	//ClockÏóê ÏûàÎçòÍ±∞
+	public void createMenus() {}
 
 	
-	public void paint(Graphics g) {};
+	public void paint(Graphics g)
+	{
+		Rectangle panelBounds = getBounds();
+		Rectangle clipBounds  = g.getClipBounds();
+
+		// The panel bounds is relative to the upper-left
+		// corner of the screen. Pretend that it's at (0,0)
+		panelBounds.x = 0;
+		panelBounds.y = 0;
+		gamecell.getCurrentOuterMostCell().redraw(g, panelBounds, true);		//{=Universe.redraw1}
+	}
 	
-	public void refreshNow() {};
+	
+	public void refreshNow()
+	{	SwingUtilities.invokeLater
+		(	new Runnable()
+			{	public void run()
+				{	Graphics g = getGraphics();
+					if( g == null )		// Universe not displayable
+						return;
+					try
+					{
+						Rectangle panelBounds = getBounds();
+						panelBounds.x = 0;
+						panelBounds.y = 0;
+						gamecell.getCurrentOuterMostCell().redraw(g, panelBounds, false); //{=Universe.redraw2}
+					}
+					finally
+					{	g.dispose();
+					}
+				}
+			}
+		);
+	}
 	
 
-	public void load() {};
-	public void store() {};
+	public void doLoad() {
+		try
+		{
+			FileInputStream in = new FileInputStream(
+			   Files.userSelected(".",".life","Life File","Load"));
+
+			Clock.instance().stop();		// stop the game and
+			gamecell.getCurrentOuterMostCell().clear();			// clear the board.
+
+			Storable memento = gamecell.getCurrentOuterMostCell().createMemento();
+			memento.load( in );
+			gamecell.getCurrentOuterMostCell().transfer( memento, new Point(0,0), Cell.LOAD );
+
+			in.close();
+		}
+		catch( IOException theException )
+		{	JOptionPane.showMessageDialog( null, "Read Failed!",
+					"The Game of Life", JOptionPane.ERROR_MESSAGE);
+		}
+		repaint();		
+	}
+	
+	
+	public void doStore() {
+		try
+		{
+			FileOutputStream out = new FileOutputStream(
+				  Files.userSelected(".",".life","Life File","Write"));
+
+			Clock.instance().stop();		// stop the game
+
+			Storable memento = gamecell.getCurrentOuterMostCell().createMemento();
+			gamecell.getCurrentOuterMostCell().transfer( memento, new Point(0,0), Cell.STORE );
+			memento.flush(out);
+
+			out.close();
+		}
+		catch( IOException theException )
+		{	JOptionPane.showMessageDialog( null, "Write Failed!",
+					"The Game of Life", JOptionPane.ERROR_MESSAGE);
+		}
+	}
 	
 }
