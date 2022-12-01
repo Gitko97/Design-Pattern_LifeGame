e5f32b3da5c0e6922d6a4c6e67fccbe68f615239
Gitko97 <xcvdsf8216@gmail.com>
2022-12-01 19:10:20 +0900

    GameCell 관련 수정
    interface->abstract

--- src/com/holub/life/factory/OriginalCell.java
+++ src/com/holub/life/factory/OriginalCell.java
@@ -1,34 +1,10 @@
 package com.holub.life.factory;
 
-import com.holub.life.Cell;
 
-import com.holub.life.Neighborhood;
-import com.holub.life.Resident;
+public class OriginalCell extends GameCell {
 
+    public OriginalCell(){
+        super();
+    }
 
-
-public class OriginalCell implements GameCell {
-	
-	private 		Cell  	outermostCell;
-
-	private static final int  DEFAULT_GRID_SIZE = 8;
-	
-	public Cell makeCell()
-	{	
-		outermostCell = new Neighborhood
-						(	DEFAULT_GRID_SIZE,
-							new Neighborhood
-							(	DEFAULT_GRID_SIZE,
-								new Resident()
-							)
-						);
-		
-		return outermostCell;
-	}
-	
-	
-	
-	public Cell getCurrentOuterMostCell() {
-		return outermostCell;
-	}
 }
